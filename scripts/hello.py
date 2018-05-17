9b0973204c365798af6a545fc6ba88dcc6404084
agreene <agreene@users.noreply.github.com>
2018-05-17 13:46:38 -0400

    Add logging

--- scripts/hello.py
+++ scripts/hello.py
@@ -2,8 +2,9 @@ import logging
 import time
 import logging
 import os
- 
+import sys 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 while True:
     logging.info("Hello, World!")
+    sys.stdout.write('Dive in')
     time.sleep(1)
